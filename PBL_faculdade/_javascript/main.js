57f9a153493566e5d1c7ea1ea34c48453aab6045
shell-o <shell.o@hotmail.com>
2020-05-06 11:17:19 -0300

    Foi inserido JavaScript

---
+++ PBL_faculdade/_javascript/main.js
@@ -0,0 +1,14 @@
+function validar()
+{
+    var nome = document.getElementById("nome");
+    var senha = document.getElementById("pass");
+
+    if(nome.value == "" || senha.value == "");
+    {
+        alert("Preencha todos os campos!");
+        return false;
+    }
+    else{
+
+    }
+}
