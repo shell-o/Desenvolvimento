79468c6926980d946fafd7e05f94fc07dd8c341d
shell-o <shell.o@hotmail.com>
2020-05-06 11:41:16 -0300

    Resolvido a questão dojavaScript

--- PBL_faculdade/_javascript/main.js
+++ PBL_faculdade/_javascript/main.js
@@ -3,12 +3,13 @@ function validar()
     var nome = document.getElementById("nome");
     var senha = document.getElementById("pass");
 
-    if(nome.value == "" || senha.value == "");
+    if(nome.value.trim() == "" || senha.value.trim() == "")
     {
         alert("Preencha todos os campos!");
         return false;
     }
-    else{
-
+    else
+    {
+        true;
     }
 }
